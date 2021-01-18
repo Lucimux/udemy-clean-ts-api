e25784ae40276a84f496269c4c417bc9fb842884
vicente <vicente.borja@alumno.buap.mx>
2021-01-18 15:39:14 -0600

    refactor: organize main layer folders

---
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -0,0 +1,10 @@
+import { makeLoginValidation } from './login-validation-factory'
+import { Controller } from '../../../../presentation/protocols'
+import { LoginController } from '../../../../presentation/controllers/login/login-controller'
+import { makeDbAuthentication } from "../../usecases/account/authentication/db-authentication-factory"
+import { makeLogController } from "../../decorators/log-controller-decorator-factory"
+
+export const makeLoginController = (): Controller => {
+    const controller =new LoginController(makeDbAuthentication(), makeLoginValidation())
+    return makeLogController(controller)
+}
