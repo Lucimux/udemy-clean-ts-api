56f8bd6fe2106aa25e5b7bd7e5d9bca8d4794282
vicente <vicente.borja@alumno.buap.mx>
2021-01-28 17:53:22 -0600

    feat: list surveys

---
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -0,0 +1,9 @@
+import { Controller } from '../../../../../presentation/protocols'
+import { makeLogController } from '../../../decorators/log-controller-decorator-factory'
+import { LoadSurveysController } from '../../../../../presentation/controllers/survey/load-surveys/load-surveys-controller'
+import { makeDbLoadSurveys } from '../../../usecases/survey/load-surveys/db-load-surveys'
+
+export const makeLoadSurveysController = (): Controller => {
+    const controller = new LoadSurveysController(makeDbLoadSurveys())
+    return makeLogController(controller)
+}
