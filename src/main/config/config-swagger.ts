0e9393e528ece6ee453c7fe6b18951e7f722a281
vicente <vicente.borja@alumno.buap.mx>
2021-04-28 19:24:22 -0500

    docs: swagger docs

---
+++ src/main/config/config-swagger.ts
@@ -0,0 +1,8 @@
+import swaggerConfig from '@/main/docs'
+import { serve, setup } from 'swagger-ui-express'
+import { noCache } from '@/main/middlewares/no-cache'
+import { Express } from 'express'
+
+export default (app: Express): void => {
+  app.use('/api-docs', noCache, serve, setup(swaggerConfig))
+}
